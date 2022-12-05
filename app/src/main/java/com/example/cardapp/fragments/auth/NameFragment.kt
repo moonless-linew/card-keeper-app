4ebe1e067bc54903ae75575feaafe657d5bbc7d5
moonless-linew <voroanto2004@yandex.ru>
2022-12-05 15:06:40 +0300

    name bug fix

--- app/src/main/java/com/example/cardapp/fragments/auth/NameFragment.kt
+++ app/src/main/java/com/example/cardapp/fragments/auth/NameFragment.kt
@@ -1,4 +1,5 @@
 package com.example.cardapp.fragments.auth
+
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -14,7 +15,7 @@ import com.example.cardapp.interfaces.OnAuthCompleteListener
 import com.example.cardapp.viewmodels.NameFragmentViewModel
 
 
-class NameFragment: Fragment(R.layout.fragment_name) {
+class NameFragment : Fragment(R.layout.fragment_name) {
     private val viewModel: NameFragmentViewModel by viewModels()
     private val binding by viewBinding(FragmentNameBinding::bind)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -23,37 +24,49 @@ class NameFragment: Fragment(R.layout.fragment_name) {
     }
 
 
-    private fun setupContinueButton(){
+    private fun setupContinueButton() {
         binding.continueButton.setOnClickListener {
-            binding.progressBar.visibility = View.VISIBLE
-            binding.continueButton.visibility = View.INVISIBLE
-            viewModel.registerUserWithName(binding.editName.text.toString(), object: OnAuthCompleteListener{
-                override fun onSuccess() {
-                    binding.progressBar.visibility = View.INVISIBLE
-                    activityNavController().navigateSafely(R.id.action_global_mainFlowFragment)
-                }
-                override fun onFail() {
+            if (binding.editName.text.toString().length in 1..30) {
+                binding.progressBar.visibility = View.VISIBLE
+                binding.continueButton.visibility = View.INVISIBLE
 
-                    binding.progressBar.visibility = View.INVISIBLE
-                    binding.continueButton.visibility = View.VISIBLE
-                    toastError(getString(R.string.error))
-                }
+                viewModel.registerUserWithName(
+                    binding.editName.text.toString(),
+                    object : OnAuthCompleteListener {
+                        override fun onSuccess() {
+                            binding.progressBar.visibility = View.INVISIBLE
+                            activityNavController().navigateSafely(R.id.action_global_mainFlowFragment)
+                        }
 
-                override fun onAuthFail() {
-                    binding.progressBar.visibility = View.INVISIBLE
-                    binding.continueButton.visibility = View.VISIBLE
-                    toastError(getString(R.string.auth_error))
-                }
-            })
+                        override fun onFail() {
+
+                            binding.progressBar.visibility = View.INVISIBLE
+                            binding.continueButton.visibility = View.VISIBLE
+                            toastError(getString(R.string.error))
+                        }
+
+                        override fun onAuthFail() {
+                            binding.progressBar.visibility = View.INVISIBLE
+                            binding.continueButton.visibility = View.VISIBLE
+                            toastError(getString(R.string.auth_error))
+                        }
+                    })
+            } else {
+                toastError(getString(R.string.name_error))
+            }
+
         }
 
 
     }
-    private fun toastError(msg: String){
+
+    private fun toastError(msg: String) {
         Toast.makeText(requireActivity(), msg, Toast.LENGTH_SHORT).show()
     }
+
     private fun Fragment.activityNavController() =
         requireActivity().findNavController(R.id.activityNavigationHost)
+
     private fun NavController.navigateSafely(@IdRes actionId: Int) {
         currentDestination?.getAction(actionId)?.let { navigate(actionId) }
     }
